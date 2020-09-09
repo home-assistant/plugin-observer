12e07608883b09a02b4aa2ca69cae66614f8b032
Pascal Vizeli <pvizeli@syshack.ch>
2020-09-09 22:37:34 +0200

    Update docker.sh

--- rootfs/etc/cont-init.d/docker.sh
+++ rootfs/etc/cont-init.d/docker.sh
@@ -3,4 +3,4 @@
 # Setup docker client settings
 # ==============================================================================
 
-echo "unix:///run/docker.sock" > /var/run/s6/container_environment/DOCKER_HOST
+echo "unix://run/docker.sock" > /var/run/s6/container_environment/DOCKER_HOST
